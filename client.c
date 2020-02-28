dac4cf3741d6a6ea812c90a68beec6e2e02c1edc
Min Sung Cha <mscha1@circinus-47.ics.uci.edu>
2020-02-27 18:48:56 -0800

    basic client implementation

--- client.c
+++ client.c
@@ -1,46 +1,86 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+#include <sys/socket.h>
+#include <stdint.h>
+#include <netdb.h>
+#include <stdlib.h>
 
 //MACROS
-#define MAX_LINE_NUM 200
+#define MAXLINE 200
 
-void start()
+
+int open_clientfd(char * hostname, char * port)
 {
-	char buffer[100];
+	int clientfd;
+	struct addrinfo hints, *listp, *p;
 
-	while(1)
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_NUMERICSERV;
+	hints.ai_flags |= AI_ADDRCONFIG;
+	getaddrinfo(hostname, port, &hints, &listp);
+
+	for (p=listp; p; p = p->ai_next)
 	{
-		printf("> ");
-		fgets(buffer, 100, stdin);
+		if ((clientfd = socket(p->ai_family, p->ai_socktype, 
+								p->ai_protocol)) < 0)
+			continue;
 
-		char * game_id;
-		char * field;
-
-		char * cmd = strtok(buffer, " \t\n");
-		if (strcmp(cmd, "quit") == 0)
-		{
+		if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1)
 			break;
-		}
-		else if (cmd != NULL)
-		{
-			game_id = atoi(cmd);
-			cmd = strtok(NULL, " ");
-			if (cmd != NULL)
-			{
-				field = cmd;
-			}
 
-			if (game_id == NULL || field == NULL)
-			{
-				printf("unknown\n");
-				game_id = NULL;
-				field = NULL;
-			}
-
-			printf("%d\n", game_id);
-			printf("%s\n", field);
-		}
+		close(clientfd);
 	}
+
+	freeaddrinfo(listp);
+	if (!p)
+		return -1;
+	else
+		return clientfd;
+}
+
+void start(char * host, char * port)
+{
+	char buffer[MAXLINE];
+
+	int clientfd;
+
+	clientfd = open_clientfd(host, port);
+
+	while (fgets(buffer, MAXLINE, stdin) != NULL)
+	{
+		write(clientfd, buffer, strlen(buffer));
+		read(clientfd, buffer, MAXLINE);
+		fputs(buffer, stdout);
+	}
+
+	close(clientfd);
+	exit(0);
+
+	// while(1)
+	// {
+	// 	printf("> ");
+	// 	fgets(buffer, MAXLINE, stdin);
+
+	// 	char * game_id;
+	// 	char * field;
+
+	// 	char * cmd = strtok(buffer, " \t\n");
+	// 	if (strcmp(cmd, "quit") == 0)
+	// 	{
+	// 		break;
+	// 	}
+	// 	else if (cmd != NULL)
+	// 	{
+	// 		game_id = atoi(cmd);
+	// 		cmd = strtok(NULL, " ");
+	// 		if (cmd != NULL)
+	// 		{
+	// 			field = cmd;
+	// 		}
+	// 	}
+	// }
 }
 
 int main(int argc, char * argv[])
@@ -48,7 +88,7 @@ int main(int argc, char * argv[])
     char * domainName = argv[1];
     char * portnum = atoi(argv[2]);
 
-    start();
+    start(domainName, portnum);
 
     return 0;
 }
